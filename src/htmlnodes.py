35f884af221b379e335d6a5447f9f17bbd85fde1
Eduardo Barroso <dev@edbar.xyz>
2024-07-29 18:47:11 -0300

    add: html nodes

--- src/htmlnodes.py
+++ src/htmlnodes.py
@@ -1,26 +1,26 @@
-class HTMLNode():
-    def __init__(self, tag=None, value=None, children=None, props=None):
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props={}):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError()
 
-    def props_to_html(self, props):
+    def props_to_html(self):
         props_string = ""
-        for k, v in props.items():
-            props_string += f' {k}=\"{v}\"'
+        for k, v in self.props.items():
+            props_string += f' {k}="{v}"'
         return props_string
 
     def __repr__(self):
-        return f'''
+        return f"""
         tag: {self.tag}
         value: {self.value}
         props: {self.props}
         children: {self.children}
-        '''
+        """
 
 
 # Custom class for nodes with no children
@@ -33,13 +33,31 @@ class LeafNode(HTMLNode):
             raise ValueError("Leaf node has no value")
         if self.tag is None:
             return str(self.value)
-        if self.props == " ":
-            return f'<{self.tag}>{self.value}</{self.tag}>'
-        return f'<{self.tag}{self.props}>{self.value}</{self.tag}>'
+
+        props_string = self.props_to_html()
+        return f"<{self.tag}{props_string}>{self.value}</{self.tag}>"
 
     def __repr__(self):
-        return f'''
+        return f"""
         tag: {self.tag}
         value: {self.value}
         props: {self.props}
-        '''
+        """
+
+
+# Custom class for nodes with children
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag associated with node")
+        if self.children is None:
+            raise ValueError("Parent node has no children")
+
+        children_nodes = "".join([child.to_html() for child in self.children])
+
+        props_string = self.props_to_html()
+
+        return f"<{self.tag}{props_string}>{children_nodes}</{self.tag}>"
--- src/test_htmlnodes.py
+++ src/test_htmlnodes.py
@@ -1,118 +1,57 @@
 import unittest
 
-from htmlnodes import HTMLNode
+from htmlnodes import LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def setUp(self):
-        self.node_with_all_params = HTMLNode(
-            tag='div',
-            value='content',
-            children=[HTMLNode(tag='span', value='child')],
-            props={'class': 'my-class', 'id': 'my-id'}
-        )
-        self.node_with_no_params = HTMLNode()
-        self.node_with_some_params = HTMLNode(tag='p', value='text')
 
-    def test_initialization_with_all_params(self):
-        self.assertEqual(self.node_with_all_params.tag, 'div')
-        self.assertEqual(self.node_with_all_params.value, 'content')
-        self.assertEqual(len(self.node_with_all_params.children), 1)
-        self.assertEqual(self.node_with_all_params.children[0].tag, 'span')
-        self.assertEqual(self.node_with_all_params.props, {
-                         'class': 'my-class', 'id': 'my-id'})
-
-    def test_initialization_with_no_params(self):
-        self.assertIsNone(self.node_with_no_params.tag)
-        self.assertIsNone(self.node_with_no_params.value)
-        self.assertIsNone(self.node_with_no_params.children)
-        self.assertIsNone(self.node_with_no_params.props)
-
-    def test_initialization_with_some_params(self):
-        self.assertEqual(self.node_with_some_params.tag, 'p')
-        self.assertEqual(self.node_with_some_params.value, 'text')
-        self.assertIsNone(self.node_with_some_params.children)
-        self.assertIsNone(self.node_with_some_params.props)
-
-    def test_props_to_html(self):
-        node = HTMLNode(props={'class': 'my-class', 'id': 'my-id'})
-        expected_props_string = ' class="my-class" id="my-id"'
-        self.assertEqual(node.props_to_html(node.props), expected_props_string)
-
-    def test_props_to_html_empty(self):
-        node = HTMLNode(props={})
-        expected_props_string = ''
-        self.assertEqual(node.props_to_html(node.props), expected_props_string)
-
-    def test_props_to_html_no_props(self):
-        node = HTMLNode()
-        expected_props_string = ''
-        self.assertEqual(node.props_to_html(
-            node.props or {}), expected_props_string)
-
-
-class LeafNode(HTMLNode):
-    def __init__(self, value, tag=None, props=None):
-        super().__init__(tag, value, None, props)
-
-    def to_html(self):
-        if self.value is None:
-            raise ValueError("Leaf node has no value")
-        if self.tag is None:
-            return str(self.value)
-        return f'<{self.tag}{self.props_to_html(self.props)}>{self.value}</{self.tag}>'
-
-    def __repr__(self):
-        return f'''
-        tag: {self.tag}
-        value: {self.value}
-        props: {self.props}
-        '''
-
-
-class TestLeafNode(unittest.TestCase):
-    def setUp(self):
-        self.node_with_tag_and_value = LeafNode(
-            value='Hello', tag='p', props={'class': 'greeting'})
-        self.node_with_value_only = LeafNode(value='World')
-        self.node_with_no_value = LeafNode(value=None, tag='div')
-
-    def test_initialization(self):
-        self.assertEqual(self.node_with_tag_and_value.tag, 'p')
-        self.assertEqual(self.node_with_tag_and_value.value, 'Hello')
-        self.assertEqual(self.node_with_tag_and_value.props,
-                         {'class': 'greeting'})
-
-        self.assertEqual(self.node_with_value_only.tag, None)
-        self.assertEqual(self.node_with_value_only.value, 'World')
-        self.assertEqual(self.node_with_value_only.props, None)
-
-    def test_to_html(self):
-        self.assertEqual(self.node_with_tag_and_value.to_html(),
-                         '<p class="greeting">Hello</p>')
-
-        self.assertEqual(self.node_with_value_only.to_html(), 'World')
-
-    def test_to_html_no_value(self):
+    def test_leaf_node_no_value(self):
+        """Test that LeafNode raises an error if no value is provided"""
         with self.assertRaises(ValueError):
-            self.node_with_no_value.to_html()
+            leaf = LeafNode(value=None, tag="p")
+            leaf.to_html()
 
-    def test_repr(self):
-        expected_repr_with_tag = '''
-        tag: p
-        value: Hello
-        props: {'class': 'greeting'}
-        '''
-        expected_repr_with_value_only = '''
-        tag: None
-        value: World
-        props: None
-        '''
-        self.assertEqual(repr(self.node_with_tag_and_value).strip(),
-                         expected_repr_with_tag.strip())
-        self.assertEqual(repr(self.node_with_value_only).strip(),
-                         expected_repr_with_value_only.strip())
+    def test_leaf_node_no_tag(self):
+        """Test LeafNode correctly returns the value when no tag is provided"""
+        leaf = LeafNode(value="Hello, World!")
+        self.assertEqual(leaf.to_html(), "Hello, World!")
+
+    def test_leaf_node_with_tag(self):
+        """Test that LeafNode correctly formats HTML with a tag"""
+        leaf = LeafNode(value="Hello, World!", tag="p", props={"class": "greeting"})
+        self.assertEqual(leaf.to_html(), '<p class="greeting">Hello, World!</p>')
+
+    def test_parent_node_no_tag(self):
+        """Test that ParentNode raises an error if no tag is provided"""
+        with self.assertRaises(ValueError):
+            parent = ParentNode(tag=None, children=[LeafNode(value="Child")])
+            parent.to_html()
+
+    def test_parent_node_no_children(self):
+        """Test that ParentNode raises an error if no children are provided"""
+        with self.assertRaises(ValueError):
+            parent = ParentNode(tag="div")
+            parent.to_html()
+
+    def test_parent_node_with_children(self):
+        """Test that ParentNode correctly formats HTML with children"""
+        child1 = LeafNode(value="Child 1")
+        child2 = LeafNode(value="Child 2")
+        parent = ParentNode(
+            tag="div", children=[child1, child2], props={"id": "parent"}
+        )
+        expected_html = '<div id="parent">Child 1Child 2</div>'
+        self.assertEqual(parent.to_html(), expected_html)
+
+    def test_parent_node_with_props(self):
+        """Test that ParentNode correctly formats HTML with props"""
+        child = LeafNode(value="Child", tag="span", props={"class": "child"})
+        parent = ParentNode(tag="section", children=[child], props={"id": "section"})
+        expected_html = (
+            '<section id="section"><span class="child">Child</span></section>'
+        )
+        self.assertEqual(parent.to_html(), expected_html)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
